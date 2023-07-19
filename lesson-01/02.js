b4e9988bde03d0656f6df928119727cd18018d03
YuraSS23 <nikitsenko_yura@mail.ru>
2023-07-19 14:30:07 +0300

    v2-remaster

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -2,6 +2,6 @@ let number =Math.floor(Math.random() * 100);
 if (number%2 ==0) {
     console.log(number+" :Это четное число");
 } else {
-    console.log(number+" :Это четное число");
+    console.log(number+" :Это нечетное число");
 }
 
